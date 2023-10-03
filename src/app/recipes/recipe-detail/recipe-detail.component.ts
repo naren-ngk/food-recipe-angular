204ff6a652cb9a9f6bc528d4a0d995013a40ad25
naren-ngk <narensakthiit@gmail.com>
2023-10-04 00:14:58 +0530

    Finished Routing

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -7,12 +9,24 @@ import { RecipeService } from '../recipe.service';
   templateUrl: './recipe-detail.component.html',
   styleUrls: ['./recipe-detail.component.css']
 })
-export class RecipeDetailComponent {
-  @Input() recipe: Recipe;
+export class RecipeDetailComponent implements OnInit {
+  recipe: Recipe;
+  id: number;
 
-  constructor(private recipeService: RecipeService) {}
+  constructor(private recipeService: RecipeService, private route: ActivatedRoute, private router: Router) { }
 
-  onAddtoShoppingList(){
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params['id'];
+      this.recipe = this.recipeService.getSingleRecipe(this.id);
+    });
+  }
+
+  onAddtoShoppingList() {
     this.recipeService.addIngredientsToList(this.recipe.ingredients);
+  }
+
+  onEditRecipe() {
+    this.router.navigate(['edit'], { relativeTo: this.route });
   }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,27 +8,31 @@ export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('Black Forest', 
-        'Dark as a chocolate. Yet fresh as a plum.', 
-        'https://images.pexels.com/photos/703116/pexels-photo-703116.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-        [
-            new Ingredient('Egg', 5),
-            new Ingredient('Cream', 1),
-            new Ingredient('Cherry', 2),
-        ]),
-        new Recipe('Cheese Burger', 
-        'What else you need to say?', 
-        'https://images.pexels.com/photos/1362044/pexels-photo-1362044.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-        [
-            new Ingredient('Buns', 2),
-            new Ingredient('Meat', 1),
-        ]),
+        new Recipe('Black Forest',
+            'Dark as a chocolate. Yet fresh as a plum.',
+            'https://images.pexels.com/photos/703116/pexels-photo-703116.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+            [
+                new Ingredient('Egg', 5),
+                new Ingredient('Cream', 1),
+                new Ingredient('Cherry', 2),
+            ]),
+        new Recipe('Cheese Burger',
+            'What else you need to say?',
+            'https://images.pexels.com/photos/1362044/pexels-photo-1362044.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+            [
+                new Ingredient('Buns', 2),
+                new Ingredient('Meat', 1),
+            ]),
     ];
 
-    constructor(private shoppingListService: ShoppingListService) {}
+    constructor(private shoppingListService: ShoppingListService) { }
 
     getRecipe() {
         return this.recipes.slice();
+    }
+
+    getSingleRecipe(id: number) {
+        return this.recipes[id];
     }
 
     addIngredientsToList(ingredients: Ingredient[]) {
